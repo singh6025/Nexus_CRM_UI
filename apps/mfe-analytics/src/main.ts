c408975ef281c429653b4f46ba3b17a9bf845e98
Your Name <your.email@example.com>
2026-05-17 20:54:43 +0530

    code pushing to mfe for all screens

--- apps/mfe-analytics/src/main.ts
+++ apps/mfe-analytics/src/main.ts
@@ -1,4 +1,1 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-bootstrapApplication(AppComponent, appConfig).catch(console.error);
+import('./bootstrap').catch(err => console.error(err));
